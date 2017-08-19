d0383874df98c493f086eedec5506b59d581c78b
Felix Brucker <felix.brucker@live.de>
2017-08-19 17:30:03 +0200

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 # replace placeholders
 sed -i 's/#PADDR#/'"$PADDR"'/' config.json
+sed -i 's/#HOSTNAME#/'"$HOSTNAME"'/' config.json
 sed -i 's/#PORT#/'"$PORT"'/' config.json
 sed -i 's/#KEY#/'"$KEY"'/' config.json
 
