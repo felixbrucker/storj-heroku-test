25a37886c8e28fa1959a93f50b9fe0fc1f8171ca
Felix Brucker <felix.brucker@live.de>
2017-08-19 17:25:54 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:6
+RUN apt update && apt install -y build-essential git-core
+WORKDIR /app
+RUN npm install -g storjshare-daemon
+RUN mkdir share && mkdir logs
+COPY config.json .
+COPY entrypoint.sh .
+ENTRYPOINT ["entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# replace placeholders
+sed -i 's/#PADDR#/'"$PADDR"'/' config.json
+sed -i 's/#PORT#/'"$PORT"'/' config.json
+sed -i 's/#KEY#/'"$KEY"'/' config.json
+
+# startup daemon and shares
+#storjshare daemon -F &
+#PID=$!
+storjshare start -c config.json --detached
+#wait $PID
